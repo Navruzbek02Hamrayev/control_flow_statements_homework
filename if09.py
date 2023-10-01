0c8a875e51633534c94d22c71e66d7c9725f8047
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-01 13:41:12 -0400

    done by Navruzbek

--- if09.py
+++ if09.py
@@ -9,4 +9,15 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    if a<=(a%10*10+a//10):
+        return True
+    if a<(a%10*10+a//10):
+        return False
+    if a>=(a%10*10+a//10):
+        return False
+    if a>(a%10*10+a//10):
+        return False
+    if a==(a%10*10+a//10):
+        return False
+print(main(57))
+print(main(21))
